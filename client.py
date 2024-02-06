0710149103e9a6f4be1a1527a531683b8fbab59f
Dulsara Dhanuka <dulsara.dhanuka@local.com>
2024-02-06 17:16:28 +0530

    server.py bug fix

--- client.py
+++ client.py
@@ -1,20 +1,30 @@
 import json
+from base64 import b64decode
 import requests
+from playsound import playsound
 
 #API_ENDPOINT = "http://localhost:5000/generate"
-API_ENDPOINT = "https://3671-35-229-241-82.ngrok-free.app/generate"
+API_ENDPOINT = "https://9f32-35-229-241-82.ngrok-free.app/generate"
 
 chat = []
 chat.append({'role': 'system', 'content': {'type': 'text', 'data': 'You are a helpful assistant'}})
 while True:
     user_input = input("User > ")
     chat.append({'role': 'user', 'content': {'type': 'text', 'data': user_input}})
-    response = requests.post(url=API_ENDPOINT, json={'chat': chat, 'voice': False}, headers={'Content-type': 'application/json'})
-    #response = json.loads(response.text)
-    #chat.append(response)
+    response = requests.post(url=API_ENDPOINT, json={'chat': chat, 'voice': True}, headers={'Content-type': 'application/json'})
     print(response.text)
-    #if response['content']['type'] == 'text':
-    #    print(f"Assistant > {response['content']['data']}")
-    #else:
-    #    print("Unknown response")
+    response = json.loads(response.text)
+    if response['content']['type'] == 'text':
+        print(f"Assistant > {response['content']['data']}")
+        chat.append({'role': 'assistant', 'content': {'type': 'text', 'data': response['content']['data']}})
+    elif response['content']['type'] == 'audio':
+        print(f"Assistant > {response['content']['text']}")
+        chat.append({'role': 'assistant', 'content': {'type': 'text', 'data': response['content']['text']}})
+        fname = f"{time.time()}.wav"
+        with open(fname, 'w+') as f:
+            f.write(b64decode(response['content']['data']))
+        playsound(fname)
+    else:
+        print('Unknown data received')
+        print(response)
 
--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import os
 from base64 import b64encode
 from llama_cpp import Llama
 from flask import Flask, abort, request, jsonify
@@ -22,6 +23,8 @@ def generate():
     context = ""
     chat = request.json['chat']
     voice = request.json['voice']
+    print("AAA" if voice else "TTT")
+    exit(0)
     for entry in chat:
         c = f"<|im_start|>{entry['role']}\n"
         if entry['content']['type'] == 'text':
